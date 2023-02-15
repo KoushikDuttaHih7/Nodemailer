717006f5cad0fcf6b25de86972a42b0877f4f705
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-15 10:57:59 +0530

    sendMail controller

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const sendMail = require("./controllers/sendMail");
+
 app.get("/", (req, res) => res.send("Hello Server!"));
+
+app.get("/mail", sendMail);
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
