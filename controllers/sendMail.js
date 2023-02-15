ec8315a53413e511abfabd5d529ed33490622f45
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-15 11:03:35 +0530

    SMTP

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -1,4 +1,17 @@
+const nodemailer = require("nodemailer");
+
 const sendMail = async (req, res) => {
+  // For test sending mails
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transport = await nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
   res.send("I am sending mail");
 };
 
